267bb99f5e292dd647bdec62d6b60f0807e857cd
翁Victor <43628067+Victor81528@users.noreply.github.com>
2021-03-17 00:23:09 +0800

    -s

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <script>
 import '@/scss/_reset.css'
 import Nav from './views/Nav.vue'
-import Footer from './views/Footer.vue's
+import Footer from './views/Footer.vue'
 
 export default {
   components: {
